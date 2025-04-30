fd7e21af29f3c4a48d72350b692edc2b857a34d0
Thony <anthony.sulgaray1@gmail.com>
2025-04-30 10:33:21 -0500

    Terminando funcion inversa

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inver <- NULL
+    crear_matriz <- function(y){
+        x <<- y
+        inver <<- NULL
+        
+    }
+    mostrar_matriz <- function() x
+    guardar_inversa <- function(inversa) inver <<- inversa
+    mostrar_inversa <- function() inver
+    list(crear_matriz = crear_matriz, mostrar_matriz = mostrar_matriz,
+         guardar_inversa = guardar_inversa, mostrar_inversa = mostrar_inversa)
 }
 
 
@@ -13,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inver <- x$mostrar_inversa()
+    if(!is.null(inver)){
+        print("Esta es la inversa")
+        return(inver)
+    }
+    data <- x$mostrar_matriz()
+    inver <- solve(data, ...)
+    x$guardar_inversa(inver)
+    inver
+    
 }
+
